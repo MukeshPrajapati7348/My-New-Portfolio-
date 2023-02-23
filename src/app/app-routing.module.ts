f6f38986d4d527a3c2fe1394ccd3259df69627c7
Mukesh Prajapati <mukeshprajapat7348@gmail.com>
2023-02-23 19:54:48 +0530

    intial commit to my portfolio application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent, pathMatch:'full'},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'skill', component: SkillComponent},
+  {path:'skill', component: SkillComponent},
+  {path:'education', component: EducationComponent},
+  {path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillComponent } from './skill/skill.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EducationComponent } from './education/education.component';
+import { MyServiceService } from './my Services/my-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    SkillComponent,
+    EducationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [MyServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
