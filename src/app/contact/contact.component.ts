dc419c54d049db8b819068ff86e7979fe1c175e1
Mukesh Prajapati <mukeshprajapat7348@gmail.com>
2023-03-20 14:25:08 +0530

    my second commit with some added comments

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
